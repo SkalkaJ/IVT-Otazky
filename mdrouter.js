41d11f36ad871683c1789723a280edf5c6731928
Daniel Sykora <daniel.sykora5@gmail.com>
2024-03-21 20:08:36 +0100

    tabulky

--- mdrouter.js
+++ mdrouter.js
@@ -1,54 +1,56 @@
 // Markdown router that renders markdown client-side using JavaScript,
-        // ensuring hyperlinks still work within the files
-        // This thing is public domain as far as I'm concerned
-        const MD = new showdown.Converter();
-        const rootpath = window.location
+// ensuring hyperlinks still work within the files
+// This thing is public domain as far as I'm concerned
+const MD = new showdown.Converter({
+    tables: true
+});
+const rootpath = window.location
 
-        function onLinkClicked(e) {
-            let href = e.target.getAttribute("href");
-            if (href.endsWith(".md")) {
-                e.preventDefault();
-                goto(href)
-            }
-        }
+function onLinkClicked(e) {
+    let href = e.target.getAttribute("href");
+    if (href.endsWith(".md")) {
+        e.preventDefault();
+        goto(href)
+    }
+}
 
-        function getCurrentUrl() {
-            let root = new URL(window.location.href)
-            let hash = decodeURIComponent(root.hash).substring(1);
-            root.hash = "";
-            root = root.toString();
+function getCurrentUrl() {
+    let root = new URL(window.location.href)
+    let hash = decodeURIComponent(root.hash).substring(1);
+    root.hash = "";
+    root = root.toString();
 
-            let current = (new URL(hash, root)).toString()
-            return current;
-        }
+    let current = (new URL(hash, root)).toString()
+    return current;
+}
 
-        function goto(where) {
-            let current = getCurrentUrl();
-            let final = new URL(where, current);
-            fetchMarkdown(final.toString())
-            window.location.hash = encodeURIComponent(final.pathname);
-        }
+function goto(where) {
+    let current = getCurrentUrl();
+    let final = new URL(where, current);
+    fetchMarkdown(final.toString())
+    window.location.hash = encodeURIComponent(final.pathname);
+}
 
-        async function fetchMarkdown(address) {
-            const mdc = document.getElementById("mdcontent")
-            mdc.innerHTML = "";
-            let f = await fetch(address);
-            let md = await f.text();
+async function fetchMarkdown(address) {
+    const mdc = document.getElementById("mdcontent")
+    mdc.innerHTML = "";
+    let f = await fetch(address);
+    let md = await f.text();
 
-            mdc.innerHTML = MD.makeHtml(md);
-            let links = document.querySelectorAll("a");
-            links.forEach(k => {
-                k.onclick = onLinkClicked;
-            })
-        }
+    mdc.innerHTML = MD.makeHtml(md);
+    let links = document.querySelectorAll("a");
+    links.forEach(k => {
+        k.onclick = onLinkClicked;
+    })
+}
 
-        function loadFromHash() {
-            let h = window.location.hash.substring(1);
-            if (!h) {
-                window.location.hash = "index.md"
-            }
-            fetchMarkdown(getCurrentUrl());
-        }
+function loadFromHash() {
+    let h = window.location.hash.substring(1);
+    if (!h) {
+        window.location.hash = "index.md"
+    }
+    fetchMarkdown(getCurrentUrl());
+}
 
-        document.addEventListener("DOMContentLoaded", loadFromHash, { once: true });
-        window.addEventListener("hashchange", loadFromHash);
+document.addEventListener("DOMContentLoaded", loadFromHash, { once: true });
+window.addEventListener("hashchange", loadFromHash);
